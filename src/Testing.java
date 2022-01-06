e2314681f0cac9fc272fe9804e1d7d5e65083dd9
AylaShiny <93674244+AylaShiny@users.noreply.github.com>
2022-01-06 21:39:44 +0530

    Update Testing.java

--- src/Testing.java
+++ src/Testing.java
@@ -2,13 +2,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
+
 
 import java.time.Duration;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class Testing {
